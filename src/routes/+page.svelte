62d82f0672ce1ddc88ce9e4160dd952200ca2a62
itsbr0dyy <1500forlifejahh@gmail.com>
2026-06-13 14:57:19 -0700

    heh

---
+++ src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  let channel = "PotatBotat";
+</script>
+
+<div class="layout">
+  <div class="stream">
+    <iframe
+      src={`https://player.twitch.tv/?channel=${channel}&parent=localhost&potat.vip`}
+      height="100%"
+      width="100%"
+      title="stream"
+      allowfullscreen
+    ></iframe>
+  </div>
+
+  <div class="chat">
+    <iframe
+      src={`https://www.twitch.tv/embed/${channel}/chat?parent=localhost&potat.vip`}
+      height="100%"
+      width="100%"
+      title="chat"
+    ></iframe>
+  </div>
+</div>
+
+<style>
+  :global(html, body) {
+    margin: 0;
+    height: 100%;
+    background: #0e0e10;
+  }
+
+  .layout {
+    display: flex;
+    height: 100vh;
+    width: 100%;
+    background: #0e0e10;
+  }
+
+  .stream {
+    flex: 3;
+    min-width: 0;
+  }
+
+  .chat {
+    flex: 1;
+    min-width: 320px;
+    border-left: 1px solid #222;
+  }
+
+  iframe {
+    width: 100%;
+    height: 100%;
+    border: 0;
+  }
+</style>
